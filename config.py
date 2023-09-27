aaa10b1b78eb07f591d0704edd3a45c0599e5ced
active <66892517+activ33@users.noreply.github.com>
2023-09-27 13:53:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Change to your bot token of @BotFather
-TOKEN = '6344846447:AAG9Yitc7Nmri61MmL5pnAWKzIOLi81AiPY'
+TOKEN = '6667225591:AAEjt37G8XZSRl2aujX7dLamzdvPJZk_1Y4'
 # Change to the id number(s) of the user(s) who is/are authorized to use the bot (@userinfobot)
 authorizedUsers = [689061386]
