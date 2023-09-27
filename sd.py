6408bc0e8c2e35da4e5d5796b3eeb9b4aded032f
active <66892517+activ33@users.noreply.github.com>
2023-09-27 13:55:05 +0530

    Add files via upload

---
+++ sd.py
@@ -0,0 +1,12 @@
+
+
+
+from pyrogram import Client
+
+app = Client("My First Project",api_id=24541704,api_hash="7b1b63a5c5a2e53233a0e78727c068d3",bot_token="6667225591:AAEjt37G8XZSRl2aujX7dLamzdvPJZk_1Y4")
+
+async def main():
+    await app.start()
+    await app.send_document(689061386, Path)
+    await app.stop()
+app.run(main())
